1dee61a6f434a60430b180e14f72e52f77aade5b
Denis Bardadym <bardadymchik@gmail.com>
2014-06-09 19:26:27 +0400

    First more or less working version

---
+++ index.js
@@ -0,0 +1,64 @@
+var should = require('should');
+var util = require('should/lib/util');
+
+Object.defineProperty(should.Assertion.prototype, 'finally', {
+  get: function() {
+    this.obj.then.should.be.a.Function;
+    return new PromisedAssertion(this.obj.then(should));
+  }
+});
+
+function PromisedAssertion(obj) {
+  this.obj = obj;
+
+  should.Assertion.apply(this, arguments);
+}
+
+PromisedAssertion.prototype = {
+  copy: should.Assertion.prototype.copy
+};
+
+function constructValueCall(name) {
+  return function() {
+    var args = arguments;
+    this.obj = this.obj.then(function(a) {
+      return a[name].apply(a, args);
+    });
+    return this;
+  }
+}
+
+function constructGetCall(name) {
+  return function() {
+    this.obj = this.obj.then(function(a) {
+      return a[name];
+    });
+
+    return this;
+  }
+}
+
+var methodsBlacklist = {
+  copy: true,
+  constructor: true,
+  getMessage: true,
+  copyIfMissing: true
+};
+
+for(var name in should.Assertion.prototype) {
+  if(name in methodsBlacklist) continue;
+  var desc = Object.getOwnPropertyDescriptor(should.Assertion.prototype, name);
+
+  if(desc.get) {
+    desc.get = constructGetCall(name);
+  } else {
+    desc.value = constructValueCall(name);
+  }
+
+  Object.defineProperty(PromisedAssertion.prototype, name, desc);
+}
+
+// delegate some methods of promises to internal promise
+PromisedAssertion.prototype.then = function(resolve, reject) {
+  return this.obj.then(resolve, reject);
+};
---
+++ test/promised.test.js
@@ -0,0 +1,37 @@
+require('../');
+
+var Promise = require('bluebird');
+
+function promised(value) {
+  return new Promise(function(resolve, reject) {
+    resolve(value);
+  })
+}
+
+describe('Should - Promised', function() {
+  describe('.finally', function() {
+    it('should return new assertion to hold promise', function() {
+      var a = promised('abc').should.finally;
+      return a.then.should.be.a.Function;
+    });
+
+
+  });
+
+  it('should allow to chain calls like with usual assertion', function() {
+    return promised('abc').should.finally.be.exactly('abc')
+      .and.be.a.String;
+  });
+
+  it('should allow to use .not and .any', function() {
+    return promised({ a: 10, b: 'abc' }).should.finally.not.have.any.of.properties('c', 'd')
+      .and.have.property('a', 10);
+  })
+
+  it('should treat assertion like promise', function() {
+    return Promise.all([
+      promised(10).should.finally.be.a.Number,
+      promised('abc').should.finally.be.a.String
+    ])
+  })
+})
